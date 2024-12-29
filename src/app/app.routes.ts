3e2774ed2325f44c7cae145201ada1bfbcb8a31b
Sabrina Bešlagić <133221791+SabrinaBeslagic@users.noreply.github.com>
2024-12-29 17:57:38 +0100

    login page - no function

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-import { KarticaComponent } from './kartica/kartica.component'
+import { KarticaComponent } from './kartica/kartica.component';
+import { LoginComponent } from './login/login.component';
 
 
 export const routes: Routes = [
  { path: '', component: HomeComponent }, // Default ruta
  { path: 'about', component: AboutComponent }, // /about prikazuje AboutComponent
- { path:'kartica',component:KarticaComponent},
- { path: '**', redirectTo: '', pathMatch: 'full' } // Fallback ruta
+ { path:'kartica',component: KarticaComponent},
+ { path: 'login', component: LoginComponent},
+ { path: '**', redirectTo: '', pathMatch: 'full' }// Fallback ruta
+
 ];
 @NgModule({
 imports: [RouterModule.forRoot(routes)],
