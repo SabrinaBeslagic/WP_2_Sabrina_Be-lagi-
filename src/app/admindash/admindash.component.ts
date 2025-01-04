fd4211c36e6a09d124a941ed5f6efa5a71f66b13
Sabrina Bešlagić <133221791+SabrinaBeslagic@users.noreply.github.com>
2025-01-04 22:59:43 +0100

    css done

---
+++ src/app/admindash/admindash.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admindash',
+  imports: [],
+  templateUrl: './admindash.component.html',
+  styleUrl: './admindash.component.css'
+})
+export class AdmindashComponent {
+
+}
